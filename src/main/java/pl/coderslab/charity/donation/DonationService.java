68643ef9af7a3608f9f38e2ced28e01cf65e3aa0
hladkipavel <pgladky22@gmail.com>
2023-03-08 16:18:33 +0100

    added donation service

---
+++ src/main/java/pl/coderslab/charity/donation/DonationService.java
@@ -0,0 +1,21 @@
+package pl.coderslab.charity.donation;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class DonationService {
+    private DonationRepository donationRepository;
+    public List<Donation> getAllDonations(){
+        return donationRepository.findAll();
+    }
+    public Donation findById(Long id){
+        return donationRepository.findById(id).orElse(null);
+    }
+    public void saveDonation(Donation donation){
+        donationRepository.save(donation);
+    }
+}
