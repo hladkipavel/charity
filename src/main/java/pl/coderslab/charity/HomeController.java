546d5c69f200f93121242d02f38163579fe27f66
hladkipavel <pgladky22@gmail.com>
2023-03-10 13:13:00 +0100

    created view all institutions on main page

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,25 @@
 package pl.coderslab.charity;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.institution.Institution;
+import pl.coderslab.charity.institution.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+    private InstitutionRepository institutionRepository;
 
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        List<Institution> institutions = institutionRepository.findAll();
+        model.addAttribute("institutions", institutions);
         return "index";
     }
 }
